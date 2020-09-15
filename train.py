4c3e252dfe5a0cf8cfebc9d5243a6d72190bbfc4
pacifictoy <ronalds@gmail.com>
2020-09-15 12:42:42 +0900

    change default learning rate
    Signed-off-by: pacifictoy <ronalds@gmail.com>

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ parser.add_argument('data_directory', action='store', type=str, help='data direc
 parser.add_argument('--gpu', action='store_true', help='use gpu, if not specified, will use cpu', default=False)
 parser.add_argument('--arch', action='store', type=str, help='base model architechture. Currently only support vgg16 or vgg11', default="vgg16")
 parser.add_argument('--epochs', action='store', type=int, help='how many epochs to train', default=1)
-parser.add_argument('--learning_rate', action='store', type=float, help='learning rate', default=0.01)
+parser.add_argument('--learning_rate', action='store', type=float, help='learning rate', default=0.001)
 parser.add_argument('--hidden_units', action='store', type=int, help='hidden unit', default=4096)
 parser.add_argument('--save_dir', action='store', type=str, help='save model into file', default='.')
 
